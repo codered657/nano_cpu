e14e302d87f21585155d3ad0b3c83c7899052675
Nicholas Ogden <nickogden91@gmail.com>
2014-02-16 00:37:46 -0800

    fixed compile errors in ALUpkg.vhd

--- src/ALU/ALUPkg.vhd
+++ src/ALU/ALUPkg.vhd
@@ -8,25 +8,30 @@
 --      Steven Okai     02/15/13    1) Initial revision.
 --                                  2) Added values for F-block ops.
 --
+library IEEE;
+use IEEE.std_logic_1164.all;
 
 package ALUPkg is
     -- ALU opcode type
     subtype alu_op is std_logic_vector(7 downto 0);
     
     -- ALU opcodes
-    constant ALU_OP_ADD  : alu_op := "XXXXXXXX"; -- ADD R1, R2
-    constant ALU_OP_SUB  : alu_op := "XXXXXXXX"; -- SUB R1, R2
+    constant ALU_OP_ADD  : alu_op := "00010000"; -- ADD R1, R2
+    constant ALU_OP_SUB  : alu_op := "00100000"; -- SUB R1, R2
     constant ALU_OP_AND  : alu_op := "00001000"; -- AND R1, R2
     constant ALU_OP_OR   : alu_op := "00001110"; -- OR R1, R2
     constant ALU_OP_XOR  : alu_op := "00000110"; -- XOR R1, R2 
-    constant ALU_OP_NEG  : alu_op := "XXXXXXXX"; -- NEG R1 
+    constant ALU_OP_NEG  : alu_op := "00110000"; -- NEG R1 
     constant ALU_OP_NOT  : alu_op := "00000011"; -- NOT R1
-    constant ALU_OP_SLL  : alu_op := "XXXXXXXX"; -- SLL R1, R2
-    constant ALU_OP_SRL  : alu_op := "XXXXXXXX"; -- SRL R1, R2
-    constant ALU_OP_SRA  : alu_op := "XXXXXXXX"; -- SRA R1, R2
-    constant ALU_OP_BSET : alu_op := "XXXXXXXX"; -- BSET R1, R2 
-    constant ALU_OP_BCLK : alu_op := "XXXXXXXX"; -- BCLR R1, R2
-    constant ALU_OP_INC  : alu_op := "XXXXXXXX"; -- INC R1
-    constant ALU_OP_DEC  : alu_op := "XXXXXXXX"; -- DEC R1
+    constant ALU_OP_SLL  : alu_op := "01000000"; -- SLL R1, R2
+    constant ALU_OP_SRL  : alu_op := "01010000"; -- SRL R1, R2
+    constant ALU_OP_SRA  : alu_op := "01100000"; -- SRA R1, R2
+    constant ALU_OP_BSET : alu_op := "01110000"; -- BSET R1, R2 
+    constant ALU_OP_BCLR : alu_op := "10000000"; -- BCLR R1, R2
+    constant ALU_OP_INC  : alu_op := "10010000"; -- INC R1
+    constant ALU_OP_DEC  : alu_op := "10100000"; -- DEC R1
+	
+	
+	constant ALU_FBLOCK_SELECT 	: std_logic_vector(3 downto 0) := "0000";  -- indicates fblock action
     
 end package ALUPkg;
