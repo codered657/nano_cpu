78f69e0a42bc3b8d9491a1a180e7a2ae1cd8efbd
Steven Okai <codered657@gmail.com>
2014-02-15 22:54:33 -0800

    - Added values for F-block ops to ALUPkg.vhd. - Added OpcodesPkg.vhd.

--- src/ALU/ALUPkg.vhd
+++ src/ALU/ALUPkg.vhd
@@ -5,7 +5,8 @@
 --  Notes: None.
 --
 --  Revision History:
---      Steven Okai     02/15/13    Initial revision.
+--      Steven Okai     02/15/13    1) Initial revision.
+--                                  2) Added values for F-block ops.
 --
 
 package ALUPkg is
@@ -15,11 +16,11 @@ package ALUPkg is
     -- ALU opcodes
     constant ALU_OP_ADD  : alu_op := "XXXXXXXX"; -- ADD R1, R2
     constant ALU_OP_SUB  : alu_op := "XXXXXXXX"; -- SUB R1, R2
-    constant ALU_OP_AND  : alu_op := "XXXXXXXX"; -- AND R1, R2
-    constant ALU_OP_OR   : alu_op := "XXXXXXXX"; -- OR R1, R2
-    constant ALU_OP_XOR  : alu_op := "XXXXXXXX"; -- XOR R1, R2 
+    constant ALU_OP_AND  : alu_op := "00001000"; -- AND R1, R2
+    constant ALU_OP_OR   : alu_op := "00001110"; -- OR R1, R2
+    constant ALU_OP_XOR  : alu_op := "00000110"; -- XOR R1, R2 
     constant ALU_OP_NEG  : alu_op := "XXXXXXXX"; -- NEG R1 
-    constant ALU_OP_NOT  : alu_op := "XXXXXXXX"; -- NOT R1
+    constant ALU_OP_NOT  : alu_op := "00000011"; -- NOT R1
     constant ALU_OP_SLL  : alu_op := "XXXXXXXX"; -- SLL R1, R2
     constant ALU_OP_SRL  : alu_op := "XXXXXXXX"; -- SRL R1, R2
     constant ALU_OP_SRA  : alu_op := "XXXXXXXX"; -- SRA R1, R2
