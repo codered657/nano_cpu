3548f80a12a04009bf48a1d0bc51c5961d6e9d3a
Steven Okai <codered657@gmail.com>
2014-02-16 23:06:43 -0800

    - Fixed compile errors in OpcodesPkg.vhd.

--- src/Packages/OpcodesPkg.vhd
+++ src/Packages/OpcodesPkg.vhd
@@ -16,50 +16,50 @@ package OpcodesPkg is
     subtype opcode is std_logic_vector(15 downto 0);
     
     -- Arithmetic opcodes.
-    constant OPCODE_ADD  : opcode := "00000000_--------";
-    constant OPCODE_SUB  : opcode := "00000001_--------";
-    constant OPCODE_AND  : opcode := "00000010_--------";
-    constant OPCODE_OR   : opcode := "00000011_--------";
-    constant OPCODE_XOR  : opcode := "00000100_--------";
-    constant OPCODE_NEG  : opcode := "00000101_--------";
-    constant OPCODE_NOT  : opcode := "00000110_--------";
-    constant OPCODE_SLL  : opcode := "00000111_--------";
-    constant OPCODE_SRL  : opcode := "00001000_--------";
-    constant OPCODE_SRA  : opcode := "00001001_--------";
-    constant OPCODE_BSET : opcode := "00001010_--------";
-    constant OPCODE_BCLK : opcode := "00001011_--------";
-    constant OPCODE_INC  : opcode := "00001100_--------";
-    constant OPCODE_DEC  : opcode := "00001101_--------";
+    constant OPCODE_ADD  : opcode := "00000000--------";
+    constant OPCODE_SUB  : opcode := "00000001--------";
+    constant OPCODE_AND  : opcode := "00000010--------";
+    constant OPCODE_OR   : opcode := "00000011--------";
+    constant OPCODE_XOR  : opcode := "00000100--------";
+    constant OPCODE_NEG  : opcode := "00000101--------";
+    constant OPCODE_NOT  : opcode := "00000110--------";
+    constant OPCODE_SLL  : opcode := "00000111--------";
+    constant OPCODE_SRL  : opcode := "00001000--------";
+    constant OPCODE_SRA  : opcode := "00001001--------";
+    constant OPCODE_BSET : opcode := "00001010--------";
+    constant OPCODE_BCLK : opcode := "00001011--------";
+    constant OPCODE_INC  : opcode := "00001100--------";
+    constant OPCODE_DEC  : opcode := "00001101--------";
     
-    constant OPCODE_LDI : opcode := "1XXX----_--------"; -- Load from immediate
-    constant OPCODE_LDM : opcode := "0XXXXXXX_--------"; -- Load from memory
-    constant OPCODE_STR : opcode := "0XXXXXXX_--------"; -- Store (from register)
-    constant OPCODE_MOV : opcode := "0XXXXXXX_--------"; -- Move (register to register)
+    constant OPCODE_LDI : opcode := "1XXX------------"; -- Load from immediate
+    constant OPCODE_LDM : opcode := "0XXXXXXX--------"; -- Load from memory
+    constant OPCODE_STR : opcode := "0XXXXXXX--------"; -- Store (from register)
+    constant OPCODE_MOV : opcode := "0XXXXXXX--------"; -- Move (register to register)
     
-    constant OPCODE_PUSH : opcode := "0XXXXXXX_XXXXXXXX";
-    constant OPCODE_POP  : opcode := "0XXXXXXX_XXXXXXXX";
+    constant OPCODE_PUSH : opcode := "0XXXXXXXXXXXXXXX";
+    constant OPCODE_POP  : opcode := "0XXXXXXXXXXXXXXX";
     
-    constant OPCODE_CALL : opcode := "0XXXXXXX_XXXXXXXX"; -- Call
-    constant OPCODE_RET  : opcode := "0XXXXXXX_XXXXXXXX"; -- Return
+    constant OPCODE_CALL : opcode := "0XXXXXXXXXXXXXXX"; -- Call
+    constant OPCODE_RET  : opcode := "0XXXXXXXXXXXXXXX"; -- Return
     
     constant OPCODE_JMP  : opcode := "0XXXXXXX_XXXXXXXX"; -- Unconditional jump
     
-    constant OPCODE_JPLT : opcode := "0XXXXXXX_--------"; -- Jump less than (signed)
-    constant OPCODE_JPGT : opcode := "0XXXXXXX_--------"; -- Jump greater than (signed)
-    constant OPCODE_JPLE : opcode := "0XXXXXXX_--------"; -- Jump less than equal (signed)
-    constant OPCODE_JPGE : opcode := "0XXXXXXX_--------"; -- Jump greater than equal (signed)
-    constant OPCODE_JPA  : opcode := "0XXXXXXX_--------"; -- Jump above (unsigned)
-    constant OPCODE_JPB  : opcode := "0XXXXXXX_--------"; -- Jump below (unsigned)
-    constant OPCODE_JPAE : opcode := "0XXXXXXX_--------"; -- Jump above equal (unsigned)
-    constant OPCODE_JPBE : opcode := "0XXXXXXX_--------"; -- Jump below equal (unsigned)
-    constant OPCODE_JPEQ : opcode := "0XXXXXXX_--------"; -- Jump equal
-    constant OPCODE_JPNE : opcode := "0XXXXXXX_--------"; -- Jump not equal
+    constant OPCODE_JPLT : opcode := "0XXXXXXX--------"; -- Jump less than (signed)
+    constant OPCODE_JPGT : opcode := "0XXXXXXX--------"; -- Jump greater than (signed)
+    constant OPCODE_JPLE : opcode := "0XXXXXXX--------"; -- Jump less than equal (signed)
+    constant OPCODE_JPGE : opcode := "0XXXXXXX--------"; -- Jump greater than equal (signed)
+    constant OPCODE_JPA  : opcode := "0XXXXXXX--------"; -- Jump above (unsigned)
+    constant OPCODE_JPB  : opcode := "0XXXXXXX--------"; -- Jump below (unsigned)
+    constant OPCODE_JPAE : opcode := "0XXXXXXX--------"; -- Jump above equal (unsigned)
+    constant OPCODE_JPBE : opcode := "0XXXXXXX--------"; -- Jump below equal (unsigned)
+    constant OPCODE_JPEQ : opcode := "0XXXXXXX--------"; -- Jump equal
+    constant OPCODE_JPNE : opcode := "0XXXXXXX--------"; -- Jump not equal
     
     
-    constant OPCODE_JPC : opcode := "0XXXXXXX_XXXXXXXX"; -- Jump carry
-    constant OPCODE_JPO : opcode := "0XXXXXXX_XXXXXXXX"; -- Jump overflow
-    constant OPCODE_JPZ : opcode := "0XXXXXXX_XXXXXXXX"; -- Jump zero
-    constant OPCODE_JPN : opcode := "0XXXXXXX_XXXXXXXX"; -- Jump negative
+    constant OPCODE_JPC : opcode := "0XXXXXXXXXXXXXXX"; -- Jump carry
+    constant OPCODE_JPO : opcode := "0XXXXXXXXXXXXXXX"; -- Jump overflow
+    constant OPCODE_JPZ : opcode := "0XXXXXXXXXXXXXXX"; -- Jump zero
+    constant OPCODE_JPN : opcode := "0XXXXXXXXXXXXXXX"; -- Jump negative
 
     
 end package OpcodesPkg;
